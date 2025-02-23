54f164cd4de0d092bce1f6fe79e3c4a2c24c7835
Luke Gronert <lukegronert@gmail.com>
2025-02-23 18:13:05 +0800

    Added functionality to draw on the grid, instead of only showing color when the mouse hovers.

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const container = document.querySelector(".container");
-let gridSize = window.prompt(
-  "What size grid do you want? Ex: 10 will give you a 10x10 grid."
-);
-document.documentElement.style.setProperty("--grid-size", gridSize);
-const buildGrid = (size) => {
+const resetBtn = document.querySelector(".reset-btn");
+
+const buildGrid = () => {
+  let gridSize = window.prompt(
+    "What size grid do you want? Ex: 10 will give you a 10x10 grid."
+  );
+  document.documentElement.style.setProperty("--grid-size", gridSize);
   let i = 0;
-  while (i < size * size) {
+  while (i < gridSize * gridSize) {
     let gridBox = document.createElement("div");
     gridBox.className = "box";
     container.appendChild(gridBox);
     i++;
   }
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) =>
+    box.addEventListener("mouseenter", () => {
+      box.classList.add("active");
+    })
+  );
 };
 
-buildGrid(gridSize);
+const resetGrid = () => {
+  let oldGrid = container.querySelectorAll(".box");
+  oldGrid.forEach((box) => box.remove());
+  buildGrid();
+};
+
+buildGrid();
+
+resetBtn.addEventListener("click", resetGrid);
